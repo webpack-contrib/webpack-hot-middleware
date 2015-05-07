5e1ceb5f2d8eb9e2bf70702af0b61dca1d1692ff
Glen Mailer <glen@stainlessed.co.uk>
2015-05-07 16:08:40 +0100

    Actually respect opts.log

--- middleware.js
+++ middleware.js
@@ -2,7 +2,7 @@ module.exports = webpackHotMiddleware;
 
 function webpackHotMiddleware(compiler, opts) {
   opts = opts || {};
-  opts.log = typeof opts.log == 'undefined' ? console.log : false;
+  opts.log = typeof opts.log == 'undefined' ? console.log : opts.log;
   opts.path = opts.path || '/__webpack_hmr';
   opts.heartbeat = opts.heartbeat || 10 * 1000;
 
