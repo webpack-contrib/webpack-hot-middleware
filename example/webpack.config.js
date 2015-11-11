1ca742866d3eda54a6365705e328db93b20ec0b5
Arnaud Limbourg <arnaudlimbourg@users.noreply.github.com>
2015-11-11 13:02:25 +0100

    chore: Add comment on using full urls in config

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -4,6 +4,8 @@ module.exports = {
   context: __dirname,
   entry: [
     // Add the client which connects to our middleware
+    // You can use full urls like 'webpack-hot-middleware/client?path=http://localhost:3000/__webpack_hmr'
+    // useful if you run your app from another point like django
     'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000',
     // And then the actual application
     './client.js'
