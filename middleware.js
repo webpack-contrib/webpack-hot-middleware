b98ccee664bf88e5d7a4877bbcaa4e9e0cfcebcb
Italo <contato@trabalha.com.br>
2018-12-21 09:31:54 -0200

    Fix Log Not Function

--- middleware.js
+++ middleware.js
@@ -5,7 +5,7 @@ var pathMatch = helpers.pathMatch;
 
 function webpackHotMiddleware(compiler, opts) {
   opts = opts || {};
-  opts.log = typeof opts.log == 'undefined' ? console.log.bind(console) : opts.log;
+  opts.log = typeof opts.log != 'undefined' && opts.log == true ? console.log.bind(console) : opts.log;
   opts.path = opts.path || '/__webpack_hmr';
   opts.heartbeat = opts.heartbeat || 10 * 1000;
 
