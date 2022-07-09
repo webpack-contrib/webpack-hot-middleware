246ddfb96fadd619f8eaf65774871b3cf3f2f9e7
chaxus <nouo18@163.com>
2022-07-09 19:12:39 +0800

    fix: fix EasyCLA, npm run lint, npm run test

--- helpers.js
+++ helpers.js
@@ -2,7 +2,8 @@ var parse = require('url').parse;
 
 exports.pathMatch = function (url, path) {
   try {
-    if (typeof URL === 'function') return new URL(url,'https://localhost/').pathname === path;
+    if (typeof URL === 'function')
+      return new URL(url, 'https://localhost/').pathname === path;
     return parse(url).pathname === path;
   } catch (e) {
     return false;
