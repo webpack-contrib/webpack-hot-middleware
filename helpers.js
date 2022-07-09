4ceb3daf6c547ebd382ce8f2f4f6184862b2e43f
chaxus <nouo18@163.com>
2022-07-09 18:54:19 +0800

    fix: fix the problem that the URL class can only resolve absolute addresses to pass the test, but I feel that "localhost" is not very good

--- helpers.js
+++ helpers.js
@@ -2,7 +2,7 @@ var parse = require('url').parse;
 
 exports.pathMatch = function (url, path) {
   try {
-    if (typeof URL === 'function') return new URL(url).pathname === path;
+    if (typeof URL === 'function') return new URL(url,'https://localhost/').pathname === path;
     return parse(url).pathname === path;
   } catch (e) {
     return false;
